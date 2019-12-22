a1d26b6680194670aaea027274a9f46c2f32f248
christophsaile <54807480+christophsaile@users.noreply.github.com>
2019-12-22 17:50:50 +0100

    add: scroll functionality for showMore

--- src/components/ShowMore/ShowMore.ts
+++ src/components/ShowMore/ShowMore.ts
@@ -1,36 +1,59 @@
-import Component from '@biotope/element';
-import template from './template';
+import Component from "@biotope/element";
+import template from "./template";
 
-import { ShowMoreProps, ShowMoreState, ShowMoreMethods } from './defines';
+import { ShowMoreProps, ShowMoreState, ShowMoreMethods } from "./defines";
 
+class ShowMore extends Component<ShowMoreProps, ShowMoreState> {
+	static componentName = "show-more";
 
+	static attributes = [];
 
-class ShowMore extends Component< ShowMoreProps, ShowMoreState > {
-    static componentName = 'show-more';
+	public methods: ShowMoreMethods = {};
+	public showMore: HTMLElement;
+	public showMoreIcon: HTMLElement;
+	public prevScrollPos: number;
+	public currentScrollPos: number;
 
-    static attributes = [
+	connectedCallback() {
+		this.showMore = this.shadowRoot.querySelector(".showMore");
+		this.showMoreIcon = this.shadowRoot.querySelector(".showMore__icon");
+		this.prevScrollPos = 100;
+		this.scrollDown();
+	}
+	public scrollDown() {
+		window.onscroll = () => {
+			this.currentScrollPos = window.pageYOffset;			
+			if (this.prevScrollPos > this.currentScrollPos) {
+				this.showMore.classList.remove("showMore--scrollActive");
+				this.showMoreIcon.classList.remove("showMore--scrollActive");
+			} else {
+				this.showMore.classList.add("showMore--scrollActive");
+				this.showMoreIcon.classList.add("showMore--scrollActive");
+			}
+		};
+		this.showMoreIcon.addEventListener("click", () => {
+			window.scrollBy({
+				top: window.innerHeight,
+				left: 0,
+				behavior: "smooth"
+			});
+		});
+	}
+	get defaultState() {
+		return {};
+	}
 
-    ];
+	get defaultProps() {
+		return {};
+	}
 
-    public methods: ShowMoreMethods = {
-
-    };
-   
-    get defaultState() {
-        return {
-
-        }
-    }
-  
-    get defaultProps() {
-        return {
-
-        }
-    }
-
-    render() {
-        return template(this.html, { ...this.props, ...this.state, ...this.methods }, this.createStyle);
-    }
+	render() {
+		return template(
+			this.html,
+			{ ...this.props, ...this.state, ...this.methods },
+			this.createStyle
+		);
+	}
 }
 
 export default ShowMore;
