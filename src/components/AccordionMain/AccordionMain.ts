9b4fd0ca9ff28fdd30c136340c393de6a37a2f9e
christophsaile <54807480+christophsaile@users.noreply.github.com>
2019-12-22 16:01:43 +0100

    feat: accordion is now working

--- src/components/AccordionMain/AccordionMain.ts
+++ src/components/AccordionMain/AccordionMain.ts
@@ -1,36 +1,58 @@
-import Component from '@biotope/element';
-import template from './template';
+import Component from "@biotope/element";
+import template from "./template";
 
-import { AccordionMainProps, AccordionMainState, AccordionMainMethods } from './defines';
+import {
+	AccordionMainProps,
+	AccordionMainState,
+	AccordionMainMethods
+} from "./defines";
 
+class AccordionMain extends Component<AccordionMainProps, AccordionMainState> {
+	static componentName = "accordion-main";
 
+	static attributes = [];
 
-class AccordionMain extends Component< AccordionMainProps, AccordionMainState > {
-    static componentName = 'accordion-main';
+	public methods: AccordionMainMethods = {};
 
-    static attributes = [
+	public accordionItems: NodeListOf<HTMLElement>;
 
-    ];
+	connectedCallback() {
+		this.accordionItems = this.shadowRoot.querySelectorAll(
+			".accordion__item"
+		);
+		this.accordionItems.forEach(item => {
+			item.classList.add("default");
+			item.addEventListener("click", (e: Event) =>
+				this.checkIfExpanded(e)
+			);
+		});
+	}
+	public checkIfExpanded = (click: Event) => {
+		this.accordionItems.forEach(elem => {
+			if (elem.classList.contains("expand")) {
+				elem.classList.remove("expand");
+				(click.target as HTMLElement).classList.add("expand");
+			} else {
+				(click.target as HTMLElement).classList.add("expand");
+			}
+		});
+	};
+	
+	get defaultState() {
+		return {};
+	}
 
-    public methods: AccordionMainMethods = {
+	get defaultProps() {
+		return {};
+	}
 
-    };
-   
-    get defaultState() {
-        return {
-
-        }
-    }
-  
-    get defaultProps() {
-        return {
-
-        }
-    }
-
-    render() {
-        return template(this.html, { ...this.props, ...this.state, ...this.methods }, this.createStyle);
-    }
+	render() {
+		return template(
+			this.html,
+			{ ...this.props, ...this.state, ...this.methods },
+			this.createStyle
+		);
+	}
 }
 
 export default AccordionMain;
--- src/components/AccordionMain/template.ts
+++ src/components/AccordionMain/template.ts
@@ -6,6 +6,12 @@ import { AccordionMainProps, AccordionMainState, AccordionMainMethods } from './
 export default (render: Function, data: AccordionMainProps & AccordionMainState & AccordionMainMethods , createStyle: Function) => {
     return render`
         ${createStyle(styles)}
-        <div>Fill me</div>
+		<section>
+			<div class="accordion__item">1</div>
+			<div class="accordion__item">2</div>
+			<div class="accordion__item">3</div>
+			<div class="accordion__item">4</div>
+			<div class="accordion__item">5</div>
+		</section>
     `;
 }
