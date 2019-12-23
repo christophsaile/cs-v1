5d9617d153f3ae3fcb04b55c1ef1a8ee8f096118
christophsaile <54807480+christophsaile@users.noreply.github.com>
2019-12-23 11:45:46 +0100

    add: markup + css for cards

--- src/components/CardComponent/template.ts
+++ src/components/CardComponent/template.ts
@@ -6,6 +6,16 @@ import { CardComponentProps, CardComponentState, CardComponentMethods } from './
 export default (render: Function, data: CardComponentProps & CardComponentState & CardComponentMethods , createStyle: Function) => {
     return render`
         ${createStyle(styles)}
-        <div>Fill me</div>
+        <article class="card">
+			<div class="card__content">
+				<div class="card__header">
+					<img class="card__img" src="https://dummyimage.com/300x150/000/fff">
+				</div>
+				<section class="card__body">
+					<h4>hello</h4>
+					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidu</p>
+				</section>
+			</div>
+		</article>
     `;
 }
