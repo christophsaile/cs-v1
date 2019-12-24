1655b436f01b22f81eebc15a66d2c4e3f181502f
christophsaile <54807480+christophsaile@users.noreply.github.com>
2019-12-24 23:55:24 +0100

    add: text for show more comp

--- src/components/ShowMore/template.ts
+++ src/components/ShowMore/template.ts
@@ -1,14 +1,18 @@
-import * as styles from './styles.scss';
+import * as styles from "./styles.scss";
 
-import { ShowMoreProps, ShowMoreState, ShowMoreMethods } from './defines';
+import { ShowMoreProps, ShowMoreState, ShowMoreMethods } from "./defines";
 
-
-export default (render: Function, data: ShowMoreProps & ShowMoreState & ShowMoreMethods , createStyle: Function) => {
-    return render`
+export default (
+	render: Function,
+	data: ShowMoreProps & ShowMoreState & ShowMoreMethods,
+	createStyle: Function
+) => {
+	return render`
 		${createStyle(styles)}
 		<div class="showMore">
-			<svg class="showMore__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-				<path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" /></svg>
+				<svg  class="showMore__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 24l-8-9h6v-15h4v15h6z"/>
+				</svg>
+				<span class="showMore__text">Learn more</span>
 		</div>
     `;
-}
+};
