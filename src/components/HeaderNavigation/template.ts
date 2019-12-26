71766be036d474ae141b64b248175c6975e81055
christophsaile <54807480+christophsaile@users.noreply.github.com>
2019-12-26 21:10:05 +0100

    add: links for nav

--- src/components/HeaderNavigation/template.ts
+++ src/components/HeaderNavigation/template.ts
@@ -10,12 +10,10 @@ export default (render: Function, data: HeaderNavigationProps & HeaderNavigation
 		<nav class="navHeader">
 		<div class="navHeader__menu-toggle"><span>Menu</span></div>
 			<ul class="navHeader__menu">
-				<li class="navHeader__links"><a href="#"><span>Home</span></a></li>
-				<li class="navHeader__links"><a href="#"><span>Learn More</span></a></li>
-				<li class="navHeader__links"><a href="#"><span>Get Involved</span></a></li>
-				<li class="navHeader__links"><a href="#"><span>Have Some Fun</span></a></li>
-				<li class="navHeader__links"><a href="#"><span>Don't Miss Out</span></a></li>
-				<li class="navHeader__links"><a href="#"><span>Get The Gear</span></a></li>
+				<li class="navHeader__links"><a href="/01site.01master.html"><span>Home</span></a></li>
+				<li class="navHeader__links"><a href="/01site.02portfolio.html"><span>Portfolio</span></a></li>
+				<li class="navHeader__links"><a href="/01site.03contact.html"><span>Contact</span></a></li>
+				<li class="navHeader__links"><a href="#"><span>Impressum</span></a></li>
 			</ul>
 		</nav>
     `;
