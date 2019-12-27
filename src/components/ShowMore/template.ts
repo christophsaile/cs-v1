da50d80ff0e36251d8da7604e52cda875e8e584d
christophsaile <54807480+christophsaile@users.noreply.github.com>
2019-12-27 13:01:00 +0100

    add: em to px

--- src/components/ShowMore/template.ts
+++ src/components/ShowMore/template.ts
@@ -10,9 +10,8 @@ export default (
 	return render`
 		${createStyle(styles)}
 		<div class="showMore">
-				<svg  class="showMore__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 24l-8-9h6v-15h4v15h6z"/>
-				</svg>
-				<span class="showMore__text">Learn more</span>
+			<svg class="showMore__icon" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M11 21.883l-6.235-7.527-.765.644 7.521 9 7.479-9-.764-.645-6.236 7.529v-21.884h-1v21.883z"/></svg>
+			<span class="showMore__text">Learn more</span>
 		</div>
     `;
 };
