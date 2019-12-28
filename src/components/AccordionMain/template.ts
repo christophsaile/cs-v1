256edcd4c7ad7726ab970efb448935fc0b133409
christophsaile <54807480+christophsaile@users.noreply.github.com>
2019-12-28 14:44:36 +0100

    add: styles for accordoin

--- src/components/AccordionMain/template.ts
+++ src/components/AccordionMain/template.ts
@@ -15,28 +15,28 @@ export default (
         ${createStyle(styles)}
 		<section class="accordion">
 			<div class="accordion__item expand">
-				<h2>Bachelor of Science OnlineMedien</h2>
-				<p>09/2018 - Heute</p>
+				<p class="accordion__date">09/2018 - Heute</p>
+				<h3 class="accordion__heading">Bachelor of Science OnlineMedien</h3>
 				<p>Hochschule für Digitale Medien in Furtwangen</p>
 			</div>
 			<div class="accordion__item">
-				<h2>Praxissemester</h2>
-				<p>09/2019 - 02/2020</p>
+				<p class="accordion__date">09/2019 - 02/2020</p>
+				<h3 class="accordion__heading">Praxissemester</h3>
 				<p>Virtual Identity AG - Freibrug</p>
 			</div>
 			<div class="accordion__item">
-				<h2>Ausbildung zum Industriekaufmann</h2>
-				<p>09/2015 - 06/2018</p>
+				<p class="accordion__date">09/2015 - 06/2018</p>
+				<h3 class="accordion__heading">Ausbildung zum Industriekaufmann</h3>
 				<p>Beton Kemmler GmbH - Hirschau</p>
 			</div>
 			<div class="accordion__item">
-				<h2>Bundesfreiwilligendienst</h2>
-				<p>09/2014 - 08/2015</p>
+				<p class="accordion__date">09/2014 - 08/2015</p>
+				<h3 class="accordion__heading">Bundesfreiwilligendienst</h3>
 				<p>Klinik für Urologie - Tübingen, Verwaltung</p>
 			</div>
 			<div class="accordion__item">
-				<h2>Allgemeine Hochschulreife</h2>
-				<p>2006 - 2014</p>
+				<p class="accordion__date"> 2006 - 2014</p>
+				<h3 class="accordion__heading">Allgemeine Hochschulreife</h3>
 				<p>Eugen-Bolz-Gymnasium - Rottenburg</p>
 			</div>
 		</section>
