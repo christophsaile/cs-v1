a7670629c6c19da8ec329a0135a054044198887a
christophsaile <54807480+christophsaile@users.noreply.github.com>
2019-12-29 13:50:43 +0100

    add: scrolling function and style

--- src/components/PageCounter/PageCounter.ts
+++ src/components/PageCounter/PageCounter.ts
@@ -1,36 +1,55 @@
-import Component from '@biotope/element';
-import template from './template';
+import Component from "@biotope/element";
+import template from "./template";
 
-import { PageCounterProps, PageCounterState, PageCounterMethods } from './defines';
+import {
+	PageCounterProps,
+	PageCounterState,
+	PageCounterMethods
+} from "./defines";
 
+class PageCounter extends Component<PageCounterProps, PageCounterState> {
+	static componentName = "page-counter";
 
+	static attributes = [];
 
-class PageCounter extends Component< PageCounterProps, PageCounterState > {
-    static componentName = 'page-counter';
+	public methods: PageCounterMethods = {};
 
-    static attributes = [
+	connectedCallback() {
+		const section = document.querySelector("section");
+		const sectionHeight = section.getBoundingClientRect().height;
+		const counterContainer = this.shadowRoot.querySelector(".counter__container");
+		const counterContainerHeight = counterContainer.getBoundingClientRect().top;
+		const calc = sectionHeight - counterContainerHeight;
 
-    ];
+		window.addEventListener("scroll", () => {
+			console.log(counterContainer.firstElementChild)
+			if(window.scrollY >= calc){
+				console.log('add');
+				counterContainer.firstElementChild.firstElementChild.classList.remove('counter__link--active');
+				counterContainer.lastElementChild.firstElementChild.classList.add('counter__link--active');
+			} else {
+				console.log('remove');
+				counterContainer.lastElementChild.firstElementChild.classList.remove('counter__link--active');
+				counterContainer.firstElementChild.firstElementChild.classList.add('counter__link--active');
+			};
+		});
+	}
 
-    public methods: PageCounterMethods = {
+	get defaultState() {
+		return {};
+	}
 
-    };
-   
-    get defaultState() {
-        return {
+	get defaultProps() {
+		return {};
+	}
 
-        }
-    }
-  
-    get defaultProps() {
-        return {
-
-        }
-    }
-
-    render() {
-        return template(this.html, { ...this.props, ...this.state, ...this.methods }, this.createStyle);
-    }
+	render() {
+		return template(
+			this.html,
+			{ ...this.props, ...this.state, ...this.methods },
+			this.createStyle
+		);
+	}
 }
 
 export default PageCounter;
--- src/components/PageCounter/template.ts
+++ src/components/PageCounter/template.ts
@@ -1,11 +1,27 @@
-import * as styles from './styles.scss';
+import * as styles from "./styles.scss";
 
-import { PageCounterProps, PageCounterState, PageCounterMethods } from './defines';
+import {
+	PageCounterProps,
+	PageCounterState,
+	PageCounterMethods
+} from "./defines";
 
-
-export default (render: Function, data: PageCounterProps & PageCounterState & PageCounterMethods , createStyle: Function) => {
-    return render`
-        ${createStyle(styles)}
-        <div>init</div>
+export default (
+	render: Function,
+	data: PageCounterProps & PageCounterState & PageCounterMethods,
+	createStyle: Function
+) => {
+	return render`
+		${createStyle(styles)}
+		<nav class="counter">
+			<ul class="counter__container">
+				<li class="counter__item"> 
+					<a class="counter__link counter__link--active" href="#">01</a>
+				</li>
+				<li class="counter__item">
+					<a class="counter__link" href="#aboutMe">02</a>
+				</li>
+			</ul>
+		</nav>
     `;
-}
+};
