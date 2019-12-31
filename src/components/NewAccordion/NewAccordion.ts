eddf629886d2b4b4f32942cded5436753626dd2d
christophsaile <54807480+christophsaile@users.noreply.github.com>
2019-12-31 13:13:19 +0100

    add: new Accordion comp

---
+++ src/components/NewAccordion/NewAccordion.ts
@@ -0,0 +1,60 @@
+import Component from "@biotope/element";
+import template from "./template";
+
+import {
+	NewAccordionProps,
+	NewAccordionState,
+	NewAccordionMethods
+} from "./defines";
+
+class NewAccordion extends Component<NewAccordionProps, NewAccordionState> {
+	static componentName = "new-accordion";
+
+	static attributes = [];
+
+	public methods: NewAccordionMethods = {};
+
+	public accordionItems: NodeListOf<HTMLElement>;
+
+	connectedCallback() {
+		const accordionContainer = this.shadowRoot.querySelectorAll(
+			".accordion"
+		);
+		accordionContainer.forEach((item: Element) => {
+			item.innerHTML = this.innerHTML;
+		});
+		const accordionItems = this.shadowRoot.querySelectorAll(
+			".accordion__item"
+		);
+		accordionItems.forEach((elem: HTMLElement) => {
+			elem.firstElementChild.addEventListener("click", function() {
+				if (this.parentElement.classList.contains("hidden")) {
+					accordionItems.forEach((elem: HTMLElement) => {
+						elem.classList.add("hidden");
+					});
+					this.parentElement.classList.remove("hidden");
+				} else {
+					this.parentElement.classList.add("hidden");
+				}
+			});
+		});
+	}
+	
+	get defaultState() {
+		return {};
+	}
+
+	get defaultProps() {
+		return {};
+	}
+
+	render() {
+		return template(
+			this.html,
+			{ ...this.props, ...this.state, ...this.methods },
+			this.createStyle
+		);
+	}
+}
+
+export default NewAccordion;
---
+++ src/components/NewAccordion/defines.ts
@@ -0,0 +1,31 @@
+/**
+ *  ## DEFINE ALL INTERFACES FOR NewAccordion
+ **/
+
+/**
+ * Props
+ */
+ interface NewAccordionProps {
+
+ }
+
+/**
+ * State
+ */
+ interface NewAccordionState {
+
+ }
+
+/**
+ * Methods
+ */
+ interface NewAccordionMethods {
+
+ }
+
+
+export {
+    NewAccordionProps,
+	NewAccordionState,
+	NewAccordionMethods
+}
