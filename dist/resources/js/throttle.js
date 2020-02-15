261d0f8a19373a3794ec4dfc9f74123154f88f7d
Christoph Saile <christoph.saile@googlemail.com>
2020-02-15 11:11:49 +0100

    add: new build

--- dist/resources/js/throttle.js
+++
@@ -1,2 +0,0 @@
-export const throttle=(t,e)=>{let o,n;return function(){const a=this,c=arguments;n?(clearTimeout(o),o=setTimeout(function(){Date.now()-n>=e&&(t.apply(a,c),n=Date.now())},e-(Date.now()-n))):(t.apply(a,c),n=Date.now())}};
-//# sourceMappingURL=throttle.js.map
