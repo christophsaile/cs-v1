c0c5b621126dcaf48ad668ca02e1c2d9899d06b7
Christoph Saile <christoph.saile@googlemail.com>
2020-03-20 14:28:42 +0100

    fixed: mobile screen about me

--- src/components/portfolio-about-me/template.ts
+++ src/components/portfolio-about-me/template.ts
@@ -15,18 +15,20 @@ export const template = (
 ): HTMLFragment => {
 	return html`
 		<section class="aboutMe" ref=${refs.scrollTriggerRef}>
-			<picture class="aboutMe__img" ref=${refs.aboutMeImgRef}>
-				<source
-					srcset="_assets/wide_${data.imgName}"
-					media="(min-width: 768px)"
-					alt=${data.imgAlt}
-				/>
-				<img
-					src="_assets/${data.imgName}"
-					alt=${data.imgAlt}
-					ref=${refs.aboutMeImgRef}
-				/>
-			</picture>
+			<div class="aboutMe__layer">
+				<picture class="aboutMe__img" ref=${refs.aboutMeImgRef}>
+					<source
+						srcset="_assets/wide_${data.imgName}"
+						media="(min-width: 768px)"
+						alt=${data.imgAlt}
+					/>
+					<img
+						src="_assets/${data.imgName}"
+						alt=${data.imgAlt}
+						ref=${refs.aboutMeImgRef}
+					/>
+				</picture>
+			</div>
 			<article class="aboutMe__content">
 				<slot />
 			</article>
