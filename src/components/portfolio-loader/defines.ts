c9b1e11f4a7d6f2394163c7ad0b144983085453b
Christoph Saile <christoph.saile@googlemail.com>
2020-03-27 09:48:50 +0100

    add: helix loader

---
+++ src/components/portfolio-loader/defines.ts
@@ -0,0 +1,25 @@
+/**
+ *  ## DEFINE ALL INTERFACES FOR PortfolioLoader
+ **/
+
+/**
+ * Props
+ */
+interface PortfolioLoaderProps {}
+
+/**
+ * State
+ */
+interface PortfolioLoaderState {}
+
+/**
+ * Methods
+ */
+interface PortfolioLoaderMethods {}
+
+
+export {
+  PortfolioLoaderProps,
+	PortfolioLoaderState,
+	PortfolioLoaderMethods
+}
